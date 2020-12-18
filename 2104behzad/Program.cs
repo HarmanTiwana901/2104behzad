e9fc90b761d70b28f4b398367b3cacfee0673b5f
Harman Tiwana <47931837+HarmanTiwana901@users.noreply.github.com>
2020-12-17 20:55:49 -0800

    added completed program.cs file

--- 2104behzad/Program.cs
+++ 2104behzad/Program.cs
@@ -1,54 +1,77 @@
 ﻿using System;
-using System.Collections.Generic;
 
-namespace _2104behzad
+namespace MyStackApp
 {
-    class Program<T>
+    class Program
     {
-        Stack<T> thestack = new Stack<T>(); // our stack
-        int stackCount = 0;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
            
-
-
-
-
+           // Stack<T> mystack = new Stack<T>(50);
         }
+    }
 
-        public int Stack(int size)
+    class Stack<T>
+    {
+        private T[] thestack;
+        private int top = -1;
+        private int stackCount = 0;
+        private int max;
+        public Stack(int size)
         {
-            return size;
+            thestack = new T[size];
+            max = size;
         }
 
         public void Push(T item)
         {
-            thestack.Push(item);
-            stackCount++;
+            // check size first before pushing
+            if (top == max - 1)
+            {
+                Console.WriteLine("Exceeded stack size amount");
+                return;
+            }
+            else
+            {
+                stackCount++;
+                thestack[++top] = item;
+            }
         }
 
         public T Pop()
         {
-            return thestack.Pop();
-            stackCount--;
+            T t = default(T);
+            if (top == -1)
+            {
+                Console.WriteLine("Empty Stack");
+                return t;
+            }
+            else
+            {
+                stackCount--;
+                return thestack[top--];
+            }
         }
 
-        /*
         public T Peek()
         {
-
+            T t = default(T);
+            // check if stack has any items
+            if (top == -1)
+            {
+                Console.WriteLine("Stack empty");
+                return t;
+            }
+            else
+            {
+                Console.WriteLine("Popped");
+                return thestack[top];
+            }
         }
-        */
+
         public int Count()
         {
             return stackCount;
-        }
-
-        public Program()
-        {
-
         }
     }
 }
