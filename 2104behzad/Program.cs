6dcec6af12bf0cd3a43c08828ca8a55ff420e925
Harman Tiwana <47931837+HarmanTiwana901@users.noreply.github.com>
2020-12-17 19:16:52 -0800

    Add files via upload

---
+++ 2104behzad/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2104behzad
+{
+    class Program<T>
+    {
+        Stack<T> thestack = new Stack<T>(); // our stack
+        int stackCount = 0;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+           
+
+
+
+
+        }
+
+        public int Stack(int size)
+        {
+            return size;
+        }
+
+        public void Push(T item)
+        {
+            thestack.Push(item);
+            stackCount++;
+        }
+
+        public T Pop()
+        {
+            return thestack.Pop();
+            stackCount--;
+        }
+
+        /*
+        public T Peek()
+        {
+
+        }
+        */
+        public int Count()
+        {
+            return stackCount;
+        }
+
+        public Program()
+        {
+
+        }
+    }
+}
